29e9b5573bda7e2ad881507786ee2538ba0521f7
Tim-s06 <timsandro.nguyen@gmail.com>
2024-09-14 11:55:08 +0200

    Resized the grid, added logic to color squares in the grid black when hovered over, added a button to change number of squares in the grid (without the logic yet)

--- script.js
+++ script.js
@@ -3,5 +3,18 @@ let container = document.getElementById("container");
 for(let i = 0;i<256;i++) {
     let squareDiv = document.createElement("div");
     squareDiv.setAttribute("id","square");
+    squareDiv.setAttribute("class","square");
     container.appendChild(squareDiv);
 }
+let squareDivs = document.querySelectorAll("div");
+squareDivs.forEach((squareDiv) => {
+    squareDiv.addEventListener("mouseenter", () => {
+        squareDiv.setAttribute("style","color: black; background: black;")
+        });
+    /*squareDiv.addEventListener("mouseleave", () => {
+        squareDiv.setAttribute("style","color: white; background: white;")
+});*/
+});
+
+    
+
