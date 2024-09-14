827d2abf2b6bba080a3eddc209bfe0ca3a21bbb7
Tim-s06 <timsandro.nguyen@gmail.com>
2024-09-14 12:59:21 +0200

    Removed the border from container div, changed the selection of divs so the container div doesnt get colored when hovered over, added logic for the user to input number after clicking the button selecting the amount of squares the grid has in each row now Problems: after changing number of squares, they change size when hovered over; browser-zoom messes with the size of the container/squares changing the amount of squares in each line

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ for(let i = 0;i<256;i++) {
     squareDiv.setAttribute("class","square");
     container.appendChild(squareDiv);
 }
-let squareDivs = document.querySelectorAll("div");
-squareDivs.forEach((squareDiv) => {
+let squareDivs = document.getElementsByClassName("square");
+console.log(squareDivs);
+Array.from(squareDivs).forEach((squareDiv) => {
     squareDiv.addEventListener("mouseenter", () => {
         squareDiv.setAttribute("style","color: black; background: black;")
         });
@@ -15,6 +16,35 @@ squareDivs.forEach((squareDiv) => {
         squareDiv.setAttribute("style","color: white; background: white;")
 });*/
 });
+let button = document.getElementById("squareNumber");
+button.addEventListener("click",  () => {
+    let input = prompt("How many squares (Max of 100)?");
+    input = Number(input);
+    if(Number.isInteger(input) && input > 0  && input <=100) {
+        console.log("inif");
+        while(container.firstChild) {
+            container.removeChild(container.lastChild);
+        }
+        for(let i = 0;i<input*input;i++) {
+            let squareDiv = document.createElement("div");
+            squareDiv.setAttribute("id","square");
+            squareDiv.setAttribute("class","square");
+            let length = Math.floor(960/input - 2);
+            squareDiv.style.height = length + "px";
+            squareDiv.style.width = length + "px";
+            container.appendChild(squareDiv);
+        }
+        
+    }
+    let newDivs = document.getElementsByClassName("square");
+    Array.from(newDivs).forEach((div) => {
+    div.addEventListener("mouseenter", () => {
+    div.setAttribute("style","background: black;")
+    });
+    });
+});
+    
+
 
     
 
