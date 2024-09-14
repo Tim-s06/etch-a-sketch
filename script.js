1861ff78c546639e646a102b830db1aac8625816
Tim-s06 <timsandro.nguyen@gmail.com>
2024-09-14 13:43:10 +0200

    Used percentage based approach to fix overflow with flex-wrap and to change the sizes of the squares correctly so that the squares fit perfectly into the container without any issues even while zooming in the browser

--- script.js
+++ script.js
@@ -2,19 +2,14 @@ let container = document.getElementById("container");
 
 for(let i = 0;i<256;i++) {
     let squareDiv = document.createElement("div");
-    squareDiv.setAttribute("id","square");
     squareDiv.setAttribute("class","square");
     container.appendChild(squareDiv);
 }
 let squareDivs = document.getElementsByClassName("square");
-console.log(squareDivs);
 Array.from(squareDivs).forEach((squareDiv) => {
     squareDiv.addEventListener("mouseenter", () => {
-        squareDiv.setAttribute("style","color: black; background: black;")
+        squareDiv.setAttribute("style","background: black;")
         });
-    /*squareDiv.addEventListener("mouseleave", () => {
-        squareDiv.setAttribute("style","color: white; background: white;")
-});*/
 });
 let button = document.getElementById("squareNumber");
 button.addEventListener("click",  () => {
@@ -22,16 +17,16 @@ button.addEventListener("click",  () => {
     input = Number(input);
     if(Number.isInteger(input) && input > 0  && input <=100) {
         console.log("inif");
+        let squarePercentage = 100 / input;
         while(container.firstChild) {
             container.removeChild(container.lastChild);
         }
         for(let i = 0;i<input*input;i++) {
             let squareDiv = document.createElement("div");
-            squareDiv.setAttribute("id","square");
             squareDiv.setAttribute("class","square");
-            let length = Math.floor(960/input - 2);
-            squareDiv.style.height = length + "px";
-            squareDiv.style.width = length + "px";
+            let length = Math.floor(960/input);
+            squareDiv.style.height = `${squarePercentage}%`;
+            squareDiv.style.width = `${squarePercentage}%`;
             container.appendChild(squareDiv);
         }
         
@@ -39,7 +34,8 @@ button.addEventListener("click",  () => {
     let newDivs = document.getElementsByClassName("square");
     Array.from(newDivs).forEach((div) => {
     div.addEventListener("mouseenter", () => {
-    div.setAttribute("style","background: black;")
+    div.style.background = "black"
+    console.log(newDivs);
     });
     });
 });
